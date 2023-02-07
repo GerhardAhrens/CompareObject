b65f6e0fdf1c66a7f9ed94bbb22dcf59600a8d5b
Gerhard Ahrens <gerhard.ahrens@pta.de>
2023-02-07 14:58:20 +0100

    First Release

---
+++ CompareObject/Comparer/CompareObject.cs
@@ -0,0 +1,111 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CompareObject.cs" company="Lifeprojects.de">
+//     Class: CompareObject
+//     Copyright © Gerhard Ahrens, 2019
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>development@lifeprojects.de</email>
+// <date>18.1.2019</date>
+//
+// <summary>Class for ObjectComparer Result</summary>
+// <example>
+//  List<CompareResult> changes = CompareObject.CompareDifferences(p1, p2);
+// </example>
+//-----------------------------------------------------------------------
+
+namespace CompareObj
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Diagnostics;
+    using System.Reflection;
+
+    [DebuggerStepThrough]
+    [Serializable]
+    public static class CompareObject
+    {
+        public static List<CompareResult> GetDifferences<T>(T firstObj, T secondObj, params string[] ignoreProperties)
+        {
+            List<CompareResult> resultCompare = new List<CompareResult>();
+            List<string> ignoreList = null;
+
+            if (firstObj == null && secondObj == null)
+            {
+                return resultCompare;
+            }
+
+            if (ignoreProperties != null)
+            {
+                ignoreList = new List<string>(ignoreProperties);
+            }
+
+            foreach (var member in typeof(T).GetMembers())
+            {
+                if (member.MemberType == MemberTypes.Property)
+                {
+                    PropertyInfo property = member as PropertyInfo;
+                    if (property != null)
+                    {
+                        string propertyType = string.Empty;
+                        if (property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                        {
+                            propertyType = $"Nullable<{property.PropertyType.GetGenericArguments()[0].Name}>";
+                        }
+                        else if (property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                        {
+                            propertyType = $"List<{property.PropertyType.GetGenericArguments()[0].Name}>";
+                        }
+                        else if (property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                        {
+                            propertyType = $"IEnumerable<{property.PropertyType.GetGenericArguments()[0].Name}>";
+                        }
+                        else if (property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(ObservableCollection<>))
+                        {
+                            propertyType = $"ObservableCollection<{property.PropertyType.GetGenericArguments()[0].Name}>";
+                        }
+                        else
+                        {
+                            propertyType = property.PropertyType.Name;
+                        }
+
+                        if (ignoreList == null)
+                        {
+                            if (property.CanRead && property.GetGetMethod().GetParameters().Length == 0)
+                            {
+                                object firstValue = firstObj == null ? null : property.GetValue(firstObj, null);
+                                object secondValue = secondObj == null ? null :  property.GetValue(secondObj, null);
+
+                                if (object.Equals(firstValue, secondValue) == false)
+                                {
+                                    resultCompare.Add(new CompareResult(firstObj.GetType().Name, property.Name, propertyType, firstValue, secondValue));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (property.CanRead && property.GetGetMethod().GetParameters().Length == 0 && ignoreList.Contains(property.Name) == false)
+                            {
+                                object firstValue = property.GetValue(firstObj, null);
+                                object secondValue = property.GetValue(secondObj, null);
+
+                                if (object.Equals(firstValue, secondValue) == false)
+                                {
+                                    resultCompare.Add(new CompareResult(firstObj.GetType().Name, property.Name, propertyType, firstValue, secondValue));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return resultCompare;
+        }
+
+        public static List<CompareResult> GetDifferences<T>(T firstObj, T secondObj)
+        {
+            return GetDifferences(firstObj, secondObj, null);
+        }
+    }
+}
---
+++ CompareObject/Comparer/CompareResult.cs
@@ -0,0 +1,111 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CompareResult.cs" company="Lifeprojects.de">
+//     Class: CompareResult
+//     Copyright © Gerhard Ahrens, 2019
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>development@lifeprojects.de</email>
+// <date>18.1.2019</date>
+//
+// <summary>Class for ObjectComparer Result</summary>
+//-----------------------------------------------------------------------
+
+namespace CompareObj
+{
+    using System;
+    using System.Diagnostics;
+
+    [DebuggerStepThrough]
+    [Serializable]
+    [DebuggerDisplay("Object={ObjectName}, PropertyName={PropertyName}, PropertyTyp={PropertyTyp}")]
+    public class CompareResult
+    {
+        public CompareResult(string name, object firstValue, object secondValue)
+        {
+            this.PropertyName = name;
+            this.FirstValue = firstValue;
+            this.SecondValue = secondValue;
+        }
+
+        public CompareResult(string objectName, string name, string propertyTyp, object firstValue, object secondValue)
+        {
+            this.ObjectName = objectName;
+            this.PropertyName = name;
+            this.PropertyTyp = propertyTyp;
+            this.FirstValue = firstValue;
+            this.SecondValue = secondValue;
+        }
+
+        public string ObjectName { get; private set; }
+
+        public string PropertyName { get; private set; }
+
+        public string PropertyTyp { get; private set; }
+
+        public object FirstValue { get; private set; }
+
+        public object SecondValue { get; private set; }
+
+        public string FullName
+        {
+            get { return $"{this.ObjectName}.({this.PropertyTyp}){this.PropertyName}; CurrentValue={this.NullToString(this.FirstValue)}; OldValue={this.NullToString(this.SecondValue)}"; }
+        }
+
+        public string ShortText
+        {
+            get
+            {
+                string op = string.Empty;
+                if (this.FirstValue == null && this.SecondValue == null)
+                {
+                    op = "==";
+                }
+                else if (this.FirstValue != null && this.SecondValue != null)
+                {
+                    op = (this.FirstValue.Equals(SecondValue) ? " == " : " != ");
+                }
+                else if (this.FirstValue != null && this.SecondValue == null)
+                {
+                    op = "!=";
+                }
+                else if (this.FirstValue == null && this.SecondValue != null)
+                {
+                    op = "!=";
+                }
+
+                string result = $"[{this.PropertyName.PadRight(20)}] : {this.NullToString(this.FirstValue)} {op} {this.NullToString(this.SecondValue)}";
+                return result;
+            }
+        }
+
+        public override string ToString()
+        {
+            string op = string.Empty;
+            if (this.FirstValue == null && this.SecondValue == null)
+            {
+                op = "==";
+            }
+            else if (this.FirstValue != null && this.SecondValue != null)
+            {
+                op = (this.FirstValue.Equals(SecondValue) ? " == " : " != ");
+            }
+            else if (this.FirstValue != null && this.SecondValue == null)
+            {
+                op = "!=";
+            }
+            else if (this.FirstValue == null && this.SecondValue != null)
+            {
+                op = "!=";
+            }
+
+            string result = $"{this.ObjectName}.{this.PropertyName} [{this.PropertyTyp}] : {NullToString(this.FirstValue)} {op} {NullToString(this.SecondValue)}";
+            return result;
+        }
+
+        private string NullToString(object value)
+        {
+            return string.IsNullOrEmpty(value.ToString()) ==true ? "null" : value.ToString();
+        }
+    }
+}
